dd17c1997f31bf857b9749213fa0607d556c8e63
github username <github email address>
2020-06-28 20:35:53 -0700

    user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tom = User.create(name: "tom")
+amina = User.create(name: "amina")
+tess = User.create(name: "tess")
